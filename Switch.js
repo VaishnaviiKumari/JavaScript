df0729ca64c34bd1a05eea8ee15aba037e604b45
Vaishnavi Kumari <132896197+VaishnaviiKumari@users.noreply.github.com>
2024-08-21 11:10:53 +0530

    Switch.js

---
+++ Switch.js
@@ -0,0 +1,51 @@
+<script>
+        let a = 10;
+        let b = 20;
+        if(a>b)
+        {
+            console.log("If block");
+        }
+        else if(a == b)
+        {
+            console.log("Else if block");
+        }
+        else
+        {
+            console.log("Else block");
+        }
+
+        nested if
+        if(a<=b)
+        {
+            if(a==b)
+            {
+                
+            }
+        }
+
+        //if ladder
+        if(a<b)
+        {
+
+        }
+        if(a>b)
+        {
+
+        }
+
+        //switch
+        switch(a)
+        {
+            case 10:
+                console.log("10 is printed");
+                break;
+            case 20:
+                console.log("20 is printed");
+                break;
+            case 30:
+                console.log("30 is printed");
+                break;
+            default:
+                console.log("default is printed");    
+        }
+    </script>
